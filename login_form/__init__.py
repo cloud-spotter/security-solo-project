04b678b1b90c5695b2932d6f8b407b49851d0d2b
cloud-spotter <cloudspotter0@gmail.com>
2024-10-25 19:50:43 +0100

    Add X-Frame-Options security header to protect against clickjacking attempts, and other security headers addressing other vulunerabilities

--- login_form/__init__.py
+++ login_form/__init__.py
@@ -36,6 +36,10 @@ def create_app(test_config=None):
     @app.after_request
     def add_security_headers(resp):
         resp.headers['Content-Security-Policy']='default-src \'self\''
+        resp.headers['X-Frame-Options'] = 'SAMEORIGIN'
+        resp.headers['X-Content-Type-Options'] = 'nosniff'
+        resp.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+        resp.headers['X-XSS-Protection'] = '1; mode=block'
         return resp
 
     return app
